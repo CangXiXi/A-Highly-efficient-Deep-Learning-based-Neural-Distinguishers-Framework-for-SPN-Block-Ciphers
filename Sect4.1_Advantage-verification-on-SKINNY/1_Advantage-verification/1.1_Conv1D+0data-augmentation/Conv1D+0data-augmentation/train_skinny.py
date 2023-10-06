0e03221b8235fd703729f5956ce09aa41a7705a4
your name <jiashuoliu@126.com>
2023-10-06 21:19:43 +0800

    SPN

--- Sect4.1_Advantage-verification-on-SKINNY/1_Advantage-verification/1.1_Conv1D+0data-augmentation/Conv1D+0data-augmentation/train_skinny.py
+++
@@ -1,8 +0,0 @@
-from train_nets import train_distinguisher
-from skinny import Skinny
-
-skinny = Skinny(n_rounds=7)
-train_distinguisher(
-    skinny, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0], n_epochs=10, case=1, lr_high=0.0011, lr_low=0.000045,
-    kernel_size=3, reg_param=0.000000043
-)
